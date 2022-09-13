884ea093de4e3e069683cc252d141646380fc19c
Odannys de La Cruz <el_odanis321@hotmail.com>
2022-09-12 21:02:34 -0500

    Crear BModal

---
+++ src/components/BModal.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+    <div class="modal">
+        <div class="modal__backdrop">
+            <div class="modal__content">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.modal {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+}
+.modal__backdrop {
+    background: rgba(0,0,0, .5);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+}
+</style>
--- src/components/BProductDetail.vue
+++ src/components/BProductDetail.vue
@@ -1,23 +1,25 @@
 <script lang="ts" setup>
-
+import { BModal } from "@/components/BModal.vue"
 </script>
 <template>
-    <div class="detail">
-        <div class="detail__content">
-            <div class="detail__image-wrapper">
-
+    <!-- <BModal> -->
+        <div class="detail">
+            <div class="detail__content">
+                <div class="detail__image-wrapper">
+    
+                </div>
+                <div class="detail__summary">
+                    <h1 class="detail__title">Kanekalon Californiano Jumbo braid </h1>
+                    <span class="detail__price">$14.000</span>
+                    <p class="detail__description">Cabello sintetico para trenzas marca jumbo brais</p>
+                </div>
             </div>
-            <div class="detail__summary">
-                <h1 class="detail__title">Kanekalon Californiano Jumbo braid </h1>
-                <span class="detail__price">$14.000</span>
-                <p class="detail__description">Cabello sintetico para trenzas marca jumbo brais</p>
+            <!-- section "recomendados, mas vendidos, productos vistos, etc" -->
+            <div class="detail__generals">
+                
             </div>
         </div>
-        <!-- section "recomendados, mas vendidos, productos vistos, etc" -->
-        <div class="detail__generals">
-
-        </div>
-    </div>
+    <!-- </BModal> -->
 </template>
 <style lang="scss" scoped>
 .product-detail {
